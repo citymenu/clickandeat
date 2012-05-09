0f7c6a9b4eb8d5b6db00055328f620f0f847f1fa
Joe Pugh <mishimaltd@gmail.com>
2012-05-09 23:20:08 +0100

    updated

---
+++ src/main/java/com/ezar/clickandeat/model/Person.java
@@ -0,0 +1,11 @@
+package com.ezar.clickandeat.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Joe Pugh
+ * Date: 09/05/12
+ * Time: 22:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
+}
