154e84fb038873e85251991c5627e16a871b0213
Joe Pugh <mishimaltd@gmail.com>
2012-06-05 12:13:42 +0100

    Updated

---
+++ src/main/webapp/resources/admin/app/EditRestaurant.js
@@ -0,0 +1,15 @@
+Ext.Loader.setConfig({enabled:true});
+
+Ext.application({
+    name: 'AD',
+    appFolder: '/resources/admin/app',
+    controllers:['RestaurantEdit'],
+
+    launch: function() {
+        Ext.create('AD.view.restaurant.Edit', {
+            width:1080,
+            height:650,
+            renderTo:'main-content'
+        });
+    }
+});
