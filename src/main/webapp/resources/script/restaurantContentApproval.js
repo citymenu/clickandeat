6235d19c371804f7702af92762966ab054ff7a41
Alberto Gonzalez <bertso@hotmail.com>
2012-10-29 16:37:00 +0000

    Alberto - Home Page Translation

--- src/main/webapp/resources/script/restaurantContentApproval.js
+++ src/main/webapp/resources/script/restaurantContentApproval.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 });
 
 
-function rejectContent() {
+function rejectContentOLD() {
    $.post( ctx+'/workflow/contentRejected.html?contentStatus='+contentStatus+'&restaurantId='+restaurantId+'&mgn=' + (Math.random() * 99999999),
        function( data ) {
            if( data.success ) {
@@ -37,4 +37,47 @@ function approveContent() {
 
     location.href = ctx + '/approval/restaurant/contentApproved.html?restaurantId='+restaurantId+'&mgn=' + (Math.random() * 99999999);
 
+}
+
+// Reject content. Present a bot to enter the reasons why the content is being rejected
+function rejectContent() {
+
+alert("NEWWW");
+
+    var header = getLabel('content.approval.reject.title');
+    var subheader = getLabel('content.approval.reject.help');
+    var content = '<textarea id=\'reasons\'></textarea>';
+    var buttons = ('<a id=\'rejectbutton\' class=\'order-button order-button-large unselectable\'>{0}</a>').format(getLabel('button.reject-content'));
+
+    var container = ('<div class=\'dialog-container\'><div class=\'dialog-header\'><h2>{0}</h2></div><div class=\'dialog-subheader\'>{1}</div><div class=\'dialog-content\'>{2}</div><div class=\'dialog-footer\'><div class=\'dialog-buttons\'>{3}</div></div></div>')
+        .format(header,subheader,content,buttons);
+
+    $.fancybox.open({
+        type: 'html',
+        content: container,
+        modal:false,
+        autoSize:false,
+        autoHeight: true,
+        width: 500,
+        openEffect:'none',
+        closeEffect:'none'
+    });
+
+    $('#rejectbutton').click(function(){
+        var rejectionReasons = $('#reasons').val();
+        $.fancybox.showLoading();
+        $.post( ctx+'/order/updateAdditionalInstructions.ajax', {
+            orderId: currentOrder.orderId,
+            additionalInstructions: $('#instructions').val()
+        },function( data ) {
+                $.fancybox.hideLoading();
+                $.fancybox.close(true);
+                if( data.success ) {
+                    buildOrder(data.order);
+                } else {
+                    alert('success:' + data.success);
+                }
+            }
+        );
+    });
 }
