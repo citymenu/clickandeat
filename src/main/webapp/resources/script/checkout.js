f433df5f3e1b55403c382bc7a89ff519a62ff997
Joe Pugh <mishimaltd@gmail.com>
2013-02-04 21:43:58 +0000

    Merge branch 'master' of github.com:mishimaltd/clickandeat
    Conflicts:
    	src/main/webapp/resources/admin/app/model/Restaurant.js

--- src/main/webapp/resources/script/checkout.js
+++ src/main/webapp/resources/script/checkout.js
@@ -179,7 +179,7 @@ function initializeMap() {
     // Add restaurant marker
     var restaurantMarker = new google.maps.Marker({
         position: restaurantLatlng,
-        icon: resources + '/images/markers/blue_MarkerB.png',
+        icon: resources + '/images/markers/green_MarkerB.png',
         map: map
     });
 
