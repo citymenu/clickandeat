a81b4c5670707f2385e43afd9571af339252aba9
Joe Pugh <mishimaltd@gmail.com>
2013-06-16 09:12:42 +0100

    SEO Refactoring

---
+++ src/test/java/com/ezar/clickandeat/repository/RestaurantDeliveryUpdateTest.java
@@ -0,0 +1,33 @@
+package com.ezar.clickandeat.repository;
+
+import com.ezar.clickandeat.model.Restaurant;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/application-context.xml"})
+public class RestaurantDeliveryUpdateTest {
+
+    @Autowired
+    private MongoOperations mongoOperations;
+
+    @Test
+    @Ignore
+    public void testUpdateDeliveryRadius() throws Exception {
+        List<Restaurant> scrapedRestaurants = mongoOperations.find(new Query(where("externalId").ne(null)),Restaurant.class);
+        for(Restaurant restaurant:scrapedRestaurants) {
+            restaurant.getDeliveryOptions().setDeliveryRadiusInKilometres(2.5d);
+            mongoOperations.save(restaurant);
+        }
+    }
+}
